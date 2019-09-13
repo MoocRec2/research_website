b01de72043504b09977e3f2e100eaff202a09a04
Anushka Yohan <aliyanage44@gmail.com>
2019-09-13 18:44:25 +0530

    Add document list - hardcoded

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+var srsListShown = false;
+var thesisListShown = false;
+$('#collapse-srs-list').hide();
+
 // Highlight a card when hovered over.
 $('.card').hover(
     function () {
@@ -8,8 +12,42 @@ $('.card').hover(
         $(this).removeClass('bg-light text-dark').addClass('bg-less border-less');
     });
 
+// Fill download button when hovered over.
+$('.download-icon').hover(
+    function () {
+        $(this).addClass('fas').removeClass('far');
+    },
+    function () {
+        $(this).addClass('far').removeClass('fas');
+    }
+)
+
 // Highlight clicked navbar item.
 $('.nav-item').click(function () {
     $('.nav-item').removeClass('active');
     $(this).addClass('active');
+});
+
+// Toggle SRS list.
+$('#toggle-srs-list').click(function () {
+   if (srsListShown) {
+       $('#collapse-srs-list').hide();
+       srsListShown = false;
+   }
+   else {
+       $('#collapse-srs-list').show();
+       srsListShown = true;
+   }
+});
+
+// Toggle Thesis list.
+$('#toggle-thesis-list').click(function () {
+    if (thesisListShown) {
+        $('#collapse-thesis-list').hide();
+        thesisListShown = false;
+    }
+    else {
+        $('#collapse-thesis-list').show();
+        thesisListShown = true;
+    }
 });
